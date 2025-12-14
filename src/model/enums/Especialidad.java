5a462bf797310f8a99a709bdde3ea4c91d80bf6a
Misael <misael.delamorena@gmail.com>
2025-12-14 17:43:21 +0100

    Documentación JavaDoc de enums, JavaDoc terminado.

--- src/model/enums/Especialidad.java
+++ src/model/enums/Especialidad.java
@@ -1,5 +1,14 @@
 package model.enums;
 
+/**
+ * Enum que representa las especialidades médicas disponibles.
+ * <p>
+ *     Cada una de las 20 constantes creadas dispone de un nombre con formato legible para imprimir por consola.
+ * </p>
+ *
+ * @author misael
+ * @version 1.0
+ */
 public enum Especialidad {
     MEDICINA_FAMILIAR("Medicina Familiar"),
     PEDIATRIA("Pediatría"),
@@ -22,16 +31,34 @@ public enum Especialidad {
     ANESTESIOLOGIA("Anestesiología"),
     RADIOLOGIA("Radiología");
 
+    /**
+     * Nombre legible de la especialidad.
+     */
     private final String nombre;
 
+    /**
+     * Constructor del enum {@code Especialidad}
+     *
+     * @param nombre Nombre legible
+     */
     Especialidad(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Devuelve el nombre legible de la especialidad.
+     *
+     * @return Nombre legible de la especialidad.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Devuelve una representación en formato texto del enum.
+     *
+     * @return Nombre legible de la especialidad.
+     */
     @Override
     public String toString() {
         return nombre;
--- src/model/enums/TipoPrueba.java
+++ src/model/enums/TipoPrueba.java
@@ -1,5 +1,14 @@
 package model.enums;
 
+/**
+ * Enum que representa los tipos de prueba disponibles.
+ * <p>
+ *     Cada constante dispone de un nombre con formato legible para imprimir por consola.
+ * </p>
+ *
+ * @author misael
+ * @version 1.0
+ */
 public enum TipoPrueba {
     ANALISIS_SANGRE("Análisis de sangre"),
     ANALISIS_ORINA("Análisis de orina"),
@@ -11,16 +20,34 @@ public enum TipoPrueba {
     PRUEBA_ESFUERZO("Prueba de esfuerzo"),
     ENDOSCOPIA("Endoscopia");
 
+    /**
+     * Nombre legible del tipo de prueba médica.
+     */
     private final String nombre;
 
+    /**
+     * Constructor del enum {@code TipoPrueba}
+     *
+     * @param nombre Nombre legible del tipo de prueba.
+     */
     TipoPrueba(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     * Devuelve el nombre legible del tipo de prueba médica.
+     *
+     * @return Nombre legible del tipo de prueba.
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     * Devuelve el nombre legible del tipo de prueba.
+     *
+     * @return Nombre legible del tipo de prueba.
+     */
     @Override
     public String toString() {
         return nombre;
